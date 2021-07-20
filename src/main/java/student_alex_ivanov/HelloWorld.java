0fb0c591604267b71b62a4c4c96323b106968c31
APTAMOH <artjom.satskih@gmail.com>
2021-07-20 22:13:41 +0300

    Art:Hello World

--- src/main/java/student_alex_ivanov/HelloWorld.java
+++ src/main/java/student_alex_ivanov/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello world");
         System.out.println("Im student ART");
         System.out.println("Jurmala one Love");
+        System.out.println("JavaGuru rules");
     }
 }
