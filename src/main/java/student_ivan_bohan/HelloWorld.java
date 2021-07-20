f0ea31cd90713ad02e31b537e9aeb7bb5231761d
numb1 <bohan.ivan@gmail.com>
2021-07-20 21:57:51 +0300

    "HelloWorld3"

--- src/main/java/student_ivan_bohan/HelloWorld.java
+++ src/main/java/student_ivan_bohan/HelloWorld.java
@@ -1,8 +1,8 @@
-package student_alex_ivanov;
+package student_ivan_bohan;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World3");
-    }
+}
 }
