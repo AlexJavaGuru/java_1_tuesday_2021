e608a6986c41a45fff31d712a1e90712e11ab574
Olegs-github <oleg-k@inbox.lv>
2021-07-20 21:55:16 +0300

    Oleg: Hello World commit

---
+++ src/main/java/student_oleg_kozlov/HelloWorld.java
@@ -0,0 +1,8 @@
+package student_oleg_kozlov;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+        System.out.println("It's a student Oleg!");
+    }
+}
