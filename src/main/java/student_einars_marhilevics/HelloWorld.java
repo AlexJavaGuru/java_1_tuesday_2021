6e4eee546e920c6e8d9a4398d8af14fe52374f57
Einars <EinarsM@gmail.com>
2021-07-20 21:55:15 +0300

    Einars: Hello World Commit

---
+++ src/main/java/student_einars_marhilevics/HelloWorld.java
@@ -0,0 +1,11 @@
+package student_einars_marhilevics;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+        System.out.println("It's Student Einar");
+        System.out.println("From JavaGuru");
+    }
+
+}
