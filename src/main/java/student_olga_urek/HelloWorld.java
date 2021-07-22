e3407c1b225fb16d20c81d036c87e01af507e5e0
NovemberCharlie1 <olga.urek@uit.no>
2021-07-22 19:52:51 +0200

    Olga: Hello World Commit

--- src/main/java/student_olga_urek/HelloWorld.java
+++ src/main/java/student_olga_urek/HelloWorld.java
@@ -3,7 +3,7 @@ package student_olga_urek;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!");
         System.out.println("I´m Olga");
         System.out.println("JavaGuru rules");
     }
