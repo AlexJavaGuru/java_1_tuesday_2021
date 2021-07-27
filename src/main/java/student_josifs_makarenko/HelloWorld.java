03952ed7bb84f13251d2e04d05fe908ab5179570
Achimari <dimandboy@mail.ru>
2021-07-27 13:10:56 +0300

    student: Josifs

---
+++ src/main/java/student_josifs_makarenko/HelloWorld.java
@@ -0,0 +1,10 @@
+package student_josifs_makarenko;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Who are you?");
+        System.out.println("Luce it's your father");
+        System.out.println("NOOOOO");
+    }
+}
