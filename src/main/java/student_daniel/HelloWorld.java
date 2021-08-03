88959832de118c0552e4976af4f91b72b96102c0
Daniel <bustaone@gmail.com>
2021-08-03 20:25:21 +0300

    variables overview

--- src/main/java/student_daniel/HelloWorld.java
+++ src/main/java/student_daniel/HelloWorld.java
@@ -1,9 +1,23 @@
 package student_daniel;
 
 public class HelloWorld {
+
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         System.out.println("It's Daniel");
         System.out.println("From Jurmala");
+
+        int age = 10;
+        age = 100;
+        System.out.println(age);
+
+        int varA = 5;
+        int varB = 10;
+
+        long firstResult;
+        boolean isHungry = true;
+        boolean isOpen = false;
+
+
     }
 }
