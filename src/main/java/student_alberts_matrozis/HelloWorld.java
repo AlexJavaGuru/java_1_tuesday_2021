57aff1bc4b61314583a97a21c270167257175975
LurkErLV <62105039+LurkErLV@users.noreply.github.com>
2021-08-03 15:15:58 +0300

    Alberts: Added all data types

--- src/main/java/student_alberts_matrozis/HelloWorld.java
+++ src/main/java/student_alberts_matrozis/HelloWorld.java
@@ -3,5 +3,18 @@ package student_alberts_matrozis;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int integer     = 5;                // 32-bit data type
+        boolean bool    = true;             // True or false
+        char character  = 't';              // 16-bit Unicode character
+        byte b          = 100;              // 8-bit data type
+        short s         = 15000;            // 16-bit data type
+        double d        = 19.22;            // 64-byte data type with floating point
+        long l          = 100000000;        // 64-bit two's complement integer
+        float f         = 1.6f;             // Single precision 32-bit data type
+        String str      = "Hello World!";   // String data type is object
+
+
+
     }
 }
