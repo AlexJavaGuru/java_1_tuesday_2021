c2f4c6d1268a922ebe33ae65845eb0f19faea836
Alex <alexoverd01@gmail.com>
2021-08-03 21:47:47 +0300

    Hot fix

--- src/main/java/student_alex_ivanov/HelloWorld.java
+++ src/main/java/student_alex_ivanov/HelloWorld.java
@@ -1,6 +1,14 @@
 package student_alex_ivanov;
 
+import teacher.annotations.CodeReview;
+import teacher.annotations.CodeReviewComment;
+
+@CodeReview(approved = false)
+@CodeReviewComment(comment = "Тут у вас что-то не правильно..." +
+        "Лучше сделать вот так:" +
+        "")
 public class HelloWorld {
+
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Im student ART");
--- src/main/java/student_alex_ivanov/lesson2/level1/Task2.java
+++
@@ -1,4 +0,0 @@
-package student_alex_ivanov.lesson2.level1;
-
-public class Task2 {
-}
