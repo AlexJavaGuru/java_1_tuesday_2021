bfabfe8ee9096c0f44fc57a00865f22686d70517
RuslanPankratov <ruslan_skaraev@mail.ru>
2021-08-04 14:06:18 +0300

    Ruslan: Hello World Commit

---
+++ src/main/java/student_ruslan_pankratov/leson_2/level_6/Task_24.java
@@ -0,0 +1,12 @@
+package student_ruslan_pankratov.leson_2.level_6;
+
+public class Task_24 {
+}
+
+
+class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
