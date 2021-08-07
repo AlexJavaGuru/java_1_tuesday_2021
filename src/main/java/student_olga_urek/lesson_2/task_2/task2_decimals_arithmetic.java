2a1c9e30ea36796eda8c53e76c62da40bd36d52b
NovemberCharlie1 <olga.urek@uit.no>
2021-08-07 20:41:54 +0200

    task2_decimals_arithmetic

---
+++ src/main/java/student_olga_urek/lesson_2/task_2/task2_decimals_arithmetic.java
@@ -0,0 +1,29 @@
+package student_olga_urek.lesson_2.task_2;
+
+import java.util.Scanner;
+
+public class task2_decimals_arithmetic {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Please enter any decimal number:");
+        double decimalA = myScanner.nextDouble();
+
+        System.out.println("Please enter another decimal number:");
+        double decimalB = myScanner.nextDouble();
+
+        double addition_result = decimalA + decimalB;
+        System.out.println(decimalA + "+"+ decimalB + "=" + addition_result);
+
+        double subtraction_result = decimalA - decimalB;
+        System.out.println(decimalA + "-"+ decimalB + "=" + subtraction_result);
+
+        double multiplication_result = decimalA * decimalB;
+        System.out.println(decimalA + "*"+ decimalB + "=" + multiplication_result);
+
+        double division_result = decimalA / decimalB;
+        System.out.println(decimalA + ":"+ decimalB + "=" + division_result);
+
+
+    }
+}
