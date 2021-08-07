6738902323c5bde3756aa36446a17761871944b4
NovemberCharlie1 <olga.urek@uit.no>
2021-08-07 21:34:50 +0200

    task_8

---
+++ src/main/java/student_olga_urek/lesson_2/level_2/task_8.java
@@ -0,0 +1,14 @@
+package student_olga_urek.lesson_2.level_2;
+
+import java.util.Scanner;
+
+public class task_8 {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Please enter your name:");
+        String userName = myScanner.nextLine();
+
+        System.out.println("Hello, "+ userName + "!");
+    }
+}
