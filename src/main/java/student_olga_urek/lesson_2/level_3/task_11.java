5035884f762fe1c795231a9622063edddb51e857
NovemberCharlie1 <olga.urek@uit.no>
2021-08-08 19:50:37 +0200

    task_11

---
+++ src/main/java/student_olga_urek/lesson_2/level_3/task_11.java
@@ -0,0 +1,19 @@
+package student_olga_urek.lesson_2.level_3;
+
+import java.util.Scanner;
+
+public class task_11 {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("This program calculates the average of any three integers. Please enter any three integers:");
+        int integerOne = myScanner.nextInt();
+        int integerTwo = myScanner.nextInt();
+        int integerThree = myScanner.nextInt();
+
+        double average = (double)(integerOne + integerTwo + integerThree) / 3;
+        System.out.println("The average of the values you entered is " + average);
+
+    }
+
+}
