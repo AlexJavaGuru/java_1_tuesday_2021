a868466649b0e51f5dbb46bb0bf1c4f8e7daeb38
NovemberCharlie1 <olga.urek@uit.no>
2021-08-08 21:16:57 +0200

    task_15

---
+++ src/main/java/student_olga_urek/lesson_2/level_5/task_15.java
@@ -0,0 +1,30 @@
+package student_olga_urek.lesson_2.level_5;
+
+import java.util.Scanner;
+
+//я не уверена, что правильно поняла задание. Может, нужно было придумать программу, где печать без переноса курсора на новую строку имеет практический смысл?
+
+public class task_15 {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Please enter your first name:");
+        String name = myScanner.nextLine();
+
+        System.out.println("Please enter your second/nick name:");
+        String nickName = myScanner.nextLine();
+
+        System.out.println("Please enter your family name:");
+        String familyName = myScanner.nextLine();
+
+        System.out.println(name);
+        System.out.println(nickName);
+        System.out.println(familyName);
+
+        System.out.print(name);
+        System.out.print(nickName);
+        System.out.print(familyName);
+
+
+    }
+}
