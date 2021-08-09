d5e63d8a10d62394dec7272f0eb599d6403726f0
AW13 <bustaone@gmail.com>
2021-08-09 22:11:54 +0300

    all 33 Task are done

--- src/main/java/student_daniel/lesson2/level5/Task_17.java
+++ src/main/java/student_daniel/lesson2/level5/Task_17.java
@@ -1,4 +1,7 @@
 package student_daniel.lesson2.level5;
 
 public class Task_17 {
+//    Посмотреть видео:  У кого нет шансов? | Рекрутер из Google
+//
+//    https://www.youtube.com/watch?v=VvRVf7_j5gQ
 }
--- src/main/java/student_daniel/lesson2/level6/Task_19.java
+++ src/main/java/student_daniel/lesson2/level6/Task_19.java
@@ -1,4 +1,9 @@
 package student_daniel.lesson2.level6;
 
 public class Task_19 {
+//    Прочитать статьи:
+//
+//    https://vc.ru/hr/98297-o-sobesedovanii-v-amazon-kakie-voprosy-budut-zadavat-na-intervyu-i-chto-nuzhno-znat-zaranee
+//
+//    https://www.inc.com/peter-economy/the-14-amazon-leadership-principles-that-can-lead-you-your-business-to-tremendous-success.html
 }
