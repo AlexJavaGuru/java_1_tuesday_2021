5c593c19d30b50e8c4411d80255c45dcdf1da4be
RuslanPankratov <ruslan_skaraev@mail.ru>
2021-08-11 13:23:09 +0300

    Ruslan: Task 1-13

---
+++ src/main/java/student_ruslan_pankratov/leson_3/level_1/Robot.java
@@ -0,0 +1,16 @@
+package student_ruslan_pankratov.leson_3.level_1;
+//Task 1
+//Task 2
+//Task 5
+public class Robot {
+    public Robot() {
+    }
+
+    public void sayHello() {
+        System.out.println("Hello");
+}
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+}
---
+++ src/main/java/student_ruslan_pankratov/leson_3/level_2/Task_8.java
@@ -0,0 +1,8 @@
+package student_ruslan_pankratov.leson_3.level_2;
+
+public class Task_8 {
+    public static void main(String[] args) {
+        String name = "Ruslan";
+        System.out.println("Hi" + name + "!");
+    }
+}
