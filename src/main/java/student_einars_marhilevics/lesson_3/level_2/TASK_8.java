154bcfb56780a9eea1c3c70a9f0aa28559b071fb
Einars <EinarsM@gmail.com>
2021-08-12 19:23:22 +0300

    Einars: level_1-2

---
+++ src/main/java/student_einars_marhilevics/lesson_3/level_2/TASK_8.java
@@ -0,0 +1,8 @@
+package student_einars_marhilevics.lesson_3.level_2;
+
+public class TASK_8 {
+    public static void main(String [] args) {
+        String name = "Einars";
+        System.out.println("Hi " + name);
+    }
+}
