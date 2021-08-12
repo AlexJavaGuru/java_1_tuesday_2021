62e8326f6d470d32aa523e3ada7d12be12e49d30
zilite <pepellv@inbox.lv>
2021-08-12 18:53:43 +0300

    1-2 u studenta artema satskih osibka

---
+++ src/main/java/student_sergej_pereligin/lesson_3/Robot.java
@@ -0,0 +1,12 @@
+package student_sergej_pereligin.lesson_3;
+
+public class Robot {
+
+    public Robot() {
+
+    }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+}
---
+++ src/main/java/student_sergej_pereligin/lesson_3/RobotDemo.java
@@ -0,0 +1,12 @@
+package student_sergej_pereligin.lesson_3;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+
+
+    Robot robot=new Robot();
+    robot.sayHello();
+}
+
+        }
+
