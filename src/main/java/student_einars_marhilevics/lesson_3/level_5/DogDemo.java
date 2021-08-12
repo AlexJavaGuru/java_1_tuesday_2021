27ecea2c3b68d89bf479364c2adbdccdab59fb97
Einars <EinarsM@gmail.com>
2021-08-12 22:24:39 +0300

    Einars: level_5

---
+++ src/main/java/student_einars_marhilevics/lesson_3/level_5/DogDemo.java
@@ -0,0 +1,12 @@
+package student_einars_marhilevics.lesson_3.level_5;
+
+public class DogDemo {
+    public static void main(String[] args) {
+        Dog dog = new Dog ("Rex", 3, "black");
+        dog.voice();
+        dog.happyBirthday();
+        dog.changeColor("white");
+        dog.voice();
+    }
+
+}
