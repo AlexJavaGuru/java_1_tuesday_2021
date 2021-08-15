2c8789d26548c09b1a6490d1bad171fdf0d45062
zilite <pepellv@inbox.lv>
2021-08-15 21:54:20 +0300

    1-2 u studenta artema satskih bila osibka so scannerom System.out napisal. ja popravil - 4tobi doma6ku prodolzit

---
+++ src/main/java/student_sergej_pereligin/lesson_3/level_2/Task_7.java
@@ -0,0 +1,9 @@
+package student_sergej_pereligin.lesson_3.level_2;
+
+public class Task_7 {
+    public static void main(String[] args) {
+        String myName = "Sergej";
+        System.out.println(myName);
+
+    }
+}
---
+++ src/main/java/student_sergej_pereligin/lesson_3/level_3/RobotDemo.java
@@ -0,0 +1,8 @@
+package student_sergej_pereligin.lesson_3.level_3;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+        Robot robot = new Robot();
+
+    }
+}
