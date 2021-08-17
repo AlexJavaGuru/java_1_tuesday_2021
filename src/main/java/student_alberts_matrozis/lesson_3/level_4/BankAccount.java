eae5f3eb85e9e9ca7b5cd98cdb73c7e92bc30649
LurkErLV <62105039+LurkErLV@users.noreply.github.com>
2021-08-17 19:36:27 +0300

    All tasks lesson 3

---
+++ src/main/java/student_alberts_matrozis/lesson_3/level_4/BankAccount.java
@@ -0,0 +1,30 @@
+package student_alberts_matrozis.lesson_3.level_4;
+
+// Task 17
+class BankAccount {
+
+    String ownerFirstName;
+    String ownerLastName;
+    int moneyAmount;
+
+    BankAccount(String ownerFirstName,
+                String ownerLastName,
+                int moneyAmount) {
+        this.ownerFirstName = ownerFirstName;
+        this.ownerLastName = ownerLastName;
+        this.moneyAmount = moneyAmount;
+    }
+
+    String getOwnerFirstName() {
+        return this.ownerFirstName;
+    }
+
+    String getOwnerLastName () {
+        return this.ownerLastName;
+    }
+
+    int getMoneyAmount() {
+        return this.moneyAmount;
+    }
+
+}
