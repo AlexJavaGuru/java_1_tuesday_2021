ff64a7a298b1d497ebcb2377027ff121af308396
Einars <EinarsM@gmail.com>
2021-08-19 23:06:02 +0300

    Einars: changes_and_lesson_4_level_1

--- src/main/java/student_einars_marhilevics/lesson_3/level_3/Robot.java
+++ src/main/java/student_einars_marhilevics/lesson_3/level_3/Robot.java
@@ -5,7 +5,11 @@ class Robot {
     Robot(String robotName) {
         this.name = robotName;
     }
-        public void sayHello() { System.out.println("Hello"); }
-        public void sayYourName() { System.out.println("My name is " + this.name); }
+        public void sayHello() {
+        System.out.println("Hello");
+    }
+        public void sayYourName() {
+        System.out.println("My name is " + this.name); 
+    }
 
 }
