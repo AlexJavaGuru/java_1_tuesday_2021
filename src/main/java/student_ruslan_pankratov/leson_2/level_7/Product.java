e436553e7659cf46861e09838fbbf76fcceb6181
RuslanPankratov <ruslan_skaraev@mail.ru>
2021-08-23 18:45:26 +0300

    Ruslan: homework fix lesson_2

--- src/main/java/student_ruslan_pankratov/leson_2/level_7/Product.java
+++ src/main/java/student_ruslan_pankratov/leson_2/level_7/Product.java
@@ -23,13 +23,3 @@ public class Product {
 }
 
 
-class ProductDemo {
-
-    public static void main(String[] args) {
-        Product product = new Product("Milk");
-        product.discount = 30;
-        product.regularPrice = 2;
-        product.printInformation();
-    }
-
-}
