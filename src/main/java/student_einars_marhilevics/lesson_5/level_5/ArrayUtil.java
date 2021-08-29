9d3552013fc7e14eb1454144c62ddf2845122bd7
Einars <EinarsM@gmail.com>
2021-08-29 13:57:04 +0300

    lesson_5_leve_2-5

--- src/main/java/student_einars_marhilevics/lesson_5/level_5/ArrayUtil.java
+++ src/main/java/student_einars_marhilevics/lesson_5/level_5/ArrayUtil.java
@@ -1,8 +1,16 @@
 package student_einars_marhilevics.lesson_5.level_5;
 
+import java.util.Random;
+
 class ArrayUtil {
     public int[] createArray(int arrayLength) {
         int[] array = new int[arrayLength];
         return array;
+    }
+    public void fillArrayWithRandomNumbers(int[] array) {
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+        }
     }
 }
--- src/main/java/student_einars_marhilevics/lesson_5/level_5/ArrayUtilTest.java
+++ src/main/java/student_einars_marhilevics/lesson_5/level_5/ArrayUtilTest.java
@@ -6,6 +6,17 @@ class ArrayUtilTest {
         test.shouldCreateArray();
     }
     public void shouldCreateArray() {
+        int arrayLength = 3;
+        int[] numbers = new int[3];
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int expectedResult = numbers.length;
+        int realResult = arrayUtil.createArray(arrayLength).length;
+        if (expectedResult == realResult) {
+            System.out.println("Test is OK");
+        } else {
+            System.out.println("Test failed");
+        }
+
         // Напишите реализацию !!!
         // Проверьте, что метод создаёт массив указанной длины
     }
