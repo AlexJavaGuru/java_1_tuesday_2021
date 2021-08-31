20d4e21599e496a70ce532ca38b38e631d1558e5
Einars <EinarsM@gmail.com>
2021-08-31 10:34:32 +0300

    hotfix

--- src/main/java/student_einars_marhilevics/lesson_4/level_6/StockTest.java
+++ src/main/java/student_einars_marhilevics/lesson_4/level_6/StockTest.java
@@ -3,64 +3,98 @@ package student_einars_marhilevics.lesson_4.level_6;
 class StockTest {
     public static void main(String[] args) {
         StockTest stockTest = new StockTest();
-        stockTest.updatePriceTest();
+        stockTest.updatePriceTestOne();
+        stockTest.updatePriceTestTwo();
+        stockTest.updatePriceTestThree();
+        stockTest.updatePriceTestFour();
+        stockTest.updatePriceTestFive();
+        stockTest.updatePriceTestSix();
     }
-    void updatePriceTest() {
+    void updatePriceTestOne() {
+        int priceOne = 10;
+        String expectedResult = "Company = Google, Current Price = 10, Min Price = 10, Max Price = 10";
+        Stock stock = new Stock("Google", 10);
+        stock.updatePrice(priceOne);
+        String realResult = stock.getPriceInformation();
+        check(expectedResult, realResult, "price one test");
+    }
+    void updatePriceTestTwo() {
+        int priceOne = 10;
+        int priceTwo = 12;
+        String expectedResult = "Company = Google, Current Price = 12, Min Price = 10, Max Price = 12";
+        Stock stock = new Stock("Google", 10);
+        stock.updatePrice(priceOne);
+        stock.updatePrice(priceTwo);
+        String realResult = stock.getPriceInformation();
+        check(expectedResult, realResult, "price two test");
+    }
+    void updatePriceTestThree() {
+        int priceOne = 10;
+        int priceTwo = 12;
+        int priceThree = 5;
+        String expectedResult = "Company = Google, Current Price = 5, Min Price = 5, Max Price = 12";
+        Stock stock = new Stock("Google", 10);
+        stock.updatePrice(priceOne);
+        stock.updatePrice(priceTwo);
+        stock.updatePrice(priceThree);
+        String realResult = stock.getPriceInformation();
+        check(expectedResult, realResult, "price three test");
+    }
+    void updatePriceTestFour() {
+        int priceOne = 10;
+        int priceTwo = 12;
+        int priceThree = 5;
+        int priceFour = 7;
+        String expectedResult = "Company = Google, Current Price = 7, Min Price = 5, Max Price = 12";
+        Stock stock = new Stock("Google", 10);
+        stock.updatePrice(priceOne);
+        stock.updatePrice(priceTwo);
+        stock.updatePrice(priceThree);
+        stock.updatePrice(priceFour);
+        String realResult = stock.getPriceInformation();
+        check(expectedResult, realResult, "price four test");
+    }
+    void updatePriceTestFive() {
+        int priceOne = 10;
+        int priceTwo = 12;
+        int priceThree = 5;
+        int priceFour = 7;
+        int priceFive = 99;
+        String expectedResult = "Company = Google, Current Price = 99, Min Price = 5, Max Price = 99";
+        Stock stock = new Stock("Google", 10);
+        stock.updatePrice(priceOne);
+        stock.updatePrice(priceTwo);
+        stock.updatePrice(priceThree);
+        stock.updatePrice(priceFour);
+        stock.updatePrice(priceFive);
+        String realResult = stock.getPriceInformation();
+        check(expectedResult, realResult, "price five test");
+    }
+    void updatePriceTestSix() {
         int priceOne = 10;
         int priceTwo = 12;
         int priceThree = 5;
         int priceFour = 7;
         int priceFive = 99;
         int priceSix = 77;
-        String expectedResultOne = "Company = Google, Current Price = 10, Min Price = 10, Max Price = 10";
-        String expectedResultTwo = "Company = Google, Current Price = 12, Min Price = 10, Max Price = 12";
-        String expectedResultThree = "Company = Google, Current Price = 5, Min Price = 5, Max Price = 12";
-        String expectedResultFour = "Company = Google, Current Price = 7, Min Price = 5, Max Price = 12";
-        String expectedResultFive = "Company = Google, Current Price = 99, Min Price = 5, Max Price = 99";
-        String expectedResultSix = "Company = Google, Current Price = 77, Min Price = 5, Max Price = 99";
-        Stock stock = new Stock("Google",10);
+        String expectedResult = "Company = Google, Current Price = 77, Min Price = 5, Max Price = 99";
+        Stock stock = new Stock("Google", 10);
         stock.updatePrice(priceOne);
+        stock.updatePrice(priceTwo);
+        stock.updatePrice(priceThree);
+        stock.updatePrice(priceFour);
+        stock.updatePrice(priceFive);
+        stock.updatePrice(priceSix);
         String realResult = stock.getPriceInformation();
-        if (realResult.equals(expectedResultOne)) {
-            System.out.println("Test updatePrice = OK");
-        } else {
-            System.out.println("Test updatePrice = Fail");
+        check(expectedResult, realResult, "price six test");
+    }
+        void check(String expectedResult, String realResult, String test) {
+            if(expectedResult.equals(realResult)) {
+                System.out.println(test + " passed");
+            } else {
+                System.out.println(test + " failed");
+            }
         }
 
-        stock.updatePrice(priceTwo);
-        realResult = stock.getPriceInformation();
-        if (realResult.equals(expectedResultTwo)) {
-            System.out.println("Test updatePrice = OK");
-        } else {
-            System.out.println("Test updatePrice = Fail");
-        }
-        stock.updatePrice(priceThree);
-        realResult = stock.getPriceInformation();
-        if (realResult.equals(expectedResultThree)) {
-            System.out.println("Test updatePrice = OK");
-        } else {
-            System.out.println("Test updatePrice = Fail");
-        }
-        stock.updatePrice(priceFour);
-        realResult = stock.getPriceInformation();
-        if (realResult.equals(expectedResultFour)) {
-            System.out.println("Test updatePrice = OK");
-        } else {
-            System.out.println("Test updatePrice = Fail");
-        }
-        stock.updatePrice(priceFive);
-        realResult = stock.getPriceInformation();
-        if (realResult.equals(expectedResultFive)) {
-            System.out.println("Test updatePrice = OK");
-        } else {
-            System.out.println("Test updatePrice = Fail");
-        }
-        stock.updatePrice(priceSix);
-        realResult = stock.getPriceInformation();
-        if (realResult.equals(expectedResultSix)) {
-            System.out.println("Test updatePrice = OK");
-        } else {
-            System.out.println("Test updatePrice = Fail");
-        }
-    }
+
 }
