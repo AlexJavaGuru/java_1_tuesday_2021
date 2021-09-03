31294ee373ef83e8205168a1cbd021f96041e25f
Einars <EinarsM@gmail.com>
2021-09-03 19:09:49 +0300

    lesson_6_level_2

--- src/main/java/student_einars_marhilevics/lesson_6/level_2/DayOfTheWeekDetectorTest.java
+++ src/main/java/student_einars_marhilevics/lesson_6/level_2/DayOfTheWeekDetectorTest.java
@@ -1,5 +1,19 @@
 package student_einars_marhilevics.lesson_6.level_2;
 
-class DayOfTheWeekDetectorTest {
-
-}
+/*class DayOfTheWeekDetectorTest {
+    DayOfTheWeekDetectorTest dayOfTheWeekDetectorTest = new DayOfTheWeekDetectorTest();
+    dayOfTheWeekDetectorTest.testMonday();
+    dayOfTheWeekDetectorTest.testTuesday();
+    dayOfTheWeekDetectorTest.testWednesday();
+    dayOfTheWeekDetectorTest.testThursday();
+    dayOfTheWeekDetectorTest.testFriday();
+    dayOfTheWeekDetectorTest.testSaturday();
+    dayOfTheWeekDetectorTest.testSunday();
+    dayOfTheWeekDetectorTest.testOther();
+    void testMonday() {
+        int dayNumber = 1;
+        DayOfTheWeekDetector DayOfTheWeekDetector = new DayOfTheWeekDetector();
+        String result = DayOfTheWeekDetector.findDayOfTheWeek(dayNumber);
+        check()
+    }
+}*/
