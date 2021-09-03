bceb3aab4674432ed30d8a317a963d13ba87f85f
Einars <EinarsM@gmail.com>
2021-09-03 15:53:34 +0300

    hotFix

--- src/main/java/student_einars_marhilevics/lesson_5/level_2/TASK_13.java
+++ src/main/java/student_einars_marhilevics/lesson_5/level_2/TASK_13.java
@@ -5,10 +5,13 @@ import java.util.Arrays;
 class TASK_13 {
     public static void main(String[] args) {
         int[] numbers = new int[3];
-        int sum = 0;
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = (int) (Math.random() * 100);
-            sum += numbers[i];
+        }
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+
         }
         System.out.println(Arrays.toString(numbers));
         System.out.println(sum);
