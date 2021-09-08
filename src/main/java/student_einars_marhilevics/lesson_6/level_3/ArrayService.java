c2bee47e0562a3de715aecf542e08ce94a11496a
Einars <EinarsM@gmail.com>
2021-09-08 21:33:55 +0300

    lesson_6_level_3

--- src/main/java/student_einars_marhilevics/lesson_6/level_3/ArrayService.java
+++ src/main/java/student_einars_marhilevics/lesson_6/level_3/ArrayService.java
@@ -1,5 +1,31 @@
 package student_einars_marhilevics.lesson_6.level_3;
 
 class ArrayService {
-
+    public boolean containsNumber(int[] array, int numberToSearch) {
+        boolean result = false;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == numberToSearch) {
+                result = true;
+            }
+        }
+        return result;
+    }
+    int countOccurrences(int[] arr, int numberToSearch) {
+        int counter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == numberToSearch) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+    boolean replaceFirst(int[] arr, int numberToReplace, int newNumber) {
+        boolean result = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == numberToReplace) {
+                result = true;
+            }
+        }
+        return result;
+    }
 }
