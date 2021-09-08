68e58bb6836cef3ecc7e3c2af26a8bf4b93f1823
Einars <EinarsM@gmail.com>
2021-09-08 22:33:55 +0300

    lesson_6_level_3

--- src/main/java/student_einars_marhilevics/lesson_6/level_3/ArrayService.java
+++ src/main/java/student_einars_marhilevics/lesson_6/level_3/ArrayService.java
@@ -1,7 +1,7 @@
 package student_einars_marhilevics.lesson_6.level_3;
 
 class ArrayService {
-    public boolean containsNumber(int[] array, int numberToSearch) {
+    boolean containsNumber(int[] array, int numberToSearch) {
         boolean result = false;
         for (int i = 0; i < array.length; i++) {
             if (array[i] == numberToSearch) {
@@ -23,9 +23,25 @@ class ArrayService {
         boolean result = false;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == numberToReplace) {
+                arr[i] = newNumber;
                 result = true;
             }
         }
         return result;
+    }
+    int[] replaceAll(int[] arr, int numberToReplace, int newNumber) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == numberToReplace) {
+                arr[i] = newNumber;
+            }
+        }
+        return arr;
+    }
+    void revert(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
     }
 }
--- src/main/java/student_einars_marhilevics/lesson_6/level_3/ArrayServiceTest.java
+++ src/main/java/student_einars_marhilevics/lesson_6/level_3/ArrayServiceTest.java
@@ -1,5 +1,16 @@
 package student_einars_marhilevics.lesson_6.level_3;
 
-class ArrayServiceTest {
+import java.util.Arrays;
 
+class ArrayServiceTest {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
