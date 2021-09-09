123eef7a563aef271635d98f2d888ef3f093bad3
zilite <pepellv@inbox.lv>
2021-09-09 21:38:41 +0300

    1,2,3,4,5 lesson
    osibka u ruslana pankratova

---
+++ src/main/java/student_sergej_pereligin/lesson_6/level_1/NumberUtil.java
@@ -0,0 +1,10 @@
+package student_sergej_pereligin.lesson_6.level_1;
+
+/*Напишите тесты для проверки метода isEven(int number).
+Упростите работу метода, код внутри может быть написан в одну строку.
+*/
+public class NumberUtil {
+    public boolean isEven(int number) {
+        return (number % 2 == 0);
+    }
+}
---
+++ src/main/java/student_sergej_pereligin/lesson_6/level_1/NumberUtilDemo.java
@@ -0,0 +1,23 @@
+package student_sergej_pereligin.lesson_6.level_1;
+
+public class NumberUtilDemo {   public static void main(String[] args) {
+    NumberUtilDemo numberUtilsTest = new NumberUtilDemo();
+    numberUtilsTest.isEvenTest();
+
+}
+
+    void isEvenTest(){
+        NumberUtil numberUtils = new NumberUtil();
+        boolean result = numberUtils.isEven(4);
+        checkRecults(result, "isEvenTest");
+    }
+
+    void checkRecults(boolean condition, String testName){
+        if (condition) {
+            System.out.println("Test " + testName + " passed!");
+        }
+        else {
+            System.out.println("Test " + testName + "  failed!");
+        }
+    }
+}
