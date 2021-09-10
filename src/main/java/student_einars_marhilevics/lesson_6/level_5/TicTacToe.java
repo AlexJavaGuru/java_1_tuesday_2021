ab1927538b1b275ce53b225d18bd7d33ef37031f
Einars <EinarsM@gmail.com>
2021-09-10 23:35:11 +0300

    lesson_6_level_5

--- src/main/java/student_einars_marhilevics/lesson_6/level_5/TicTacToe.java
+++ src/main/java/student_einars_marhilevics/lesson_6/level_5/TicTacToe.java
@@ -1,7 +1,28 @@
 package student_einars_marhilevics.lesson_6.level_5;
 
-/*class TicTacToe {
+class TicTacToe {
     boolean isWinPositionForHorizontals(int[][] field, int playerToCheck) {
-
+        boolean win = false;
+        for (int i = 0; i < field.length; i++) {
+            if (playerToCheck == field[i][0] && playerToCheck == field[i][1] && playerToCheck == field[i][2]) {
+                win = true;
+            }
+            else {
+                win = false;
+            }
+        }
+        return win;
     }
-}*/
+    boolean isWinPositionForVerticals(int[][] field, int playerToCheck) {
+        boolean win = false;
+        for (int i = 0; i < field.length; i++) {
+            if (playerToCheck == field[0][i] && playerToCheck == field[1][i] && playerToCheck == field[2][i]) {
+                win = true;
+            }
+            else {
+                win = false;
+            }
+        }
+        return win;
+    }
+}
--- src/main/java/student_einars_marhilevics/lesson_6/level_5/TicTacToeTest.java
+++ src/main/java/student_einars_marhilevics/lesson_6/level_5/TicTacToeTest.java
@@ -1,4 +1,5 @@
 package student_einars_marhilevics.lesson_6.level_5;
 
 class TicTacToeTest {
+
 }
