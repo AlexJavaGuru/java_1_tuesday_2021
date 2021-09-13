1beffeb2555fd0682d77dc78019fe0bacd2d5299
Einars <EinarsM@gmail.com>
2021-09-13 21:20:08 +0300

    lesson_7_level_6

--- src/main/java/student_einars_marhilevics/lesson_7/level_6/CreditCard.java
+++ src/main/java/student_einars_marhilevics/lesson_7/level_6/CreditCard.java
@@ -1,5 +1,57 @@
 package student_einars_marhilevics.lesson_7.level_6;
 
 class CreditCard {
+    int cardNumber;
+    int pinCode;
+    int cardBalance;
+    int creditLimit;
+    int cardDebt;
 
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public int getPinCode() {
+        return pinCode;
+    }
+
+    public int getCardBalance() {
+        return cardBalance;
+    }
+
+    public int getCreditLimit() {
+        return creditLimit;
+    }
+
+    public int getCardDebt() {
+        return cardDebt;
+    }
+    public CreditCard (int cardNumber, int pinCode) {
+        this.cardNumber = cardNumber;
+        this.pinCode = pinCode;
+        cardBalance = 0;
+        cardDebt = 0;
+        creditLimit = 0;
+    }
+    boolean deposit(int pinCode, int amount) {
+        if (this.pinCode != pinCode) {
+            return false;
+        } else {
+            if (cardDebt > 0) {
+                amount = decreaseDebt(amount);
+            }
+            if (amount > 0) {
+                cardBalance += amount;
+            }
+        }
+        return true;
+    }
+    int decreaseDebt(int amount) {
+        int dif = 0;
+        cardDebt -= amount;
+        if (cardDebt < 0) {
+            dif = cardDebt * (-1);
+        }
+        return dif;
+    }
 }
