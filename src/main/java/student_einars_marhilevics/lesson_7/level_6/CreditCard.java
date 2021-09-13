4b53e76460c65a5f64419f3c8fb87bcf754e68e3
Einars <EinarsM@gmail.com>
2021-09-13 19:40:28 +0300

    lesson_7_level_6

---
+++ src/main/java/student_einars_marhilevics/lesson_7/level_6/CreditCard.java
@@ -0,0 +1,5 @@
+package student_einars_marhilevics.lesson_7.level_6;
+
+class CreditCard {
+
+}
---
+++ src/main/java/student_einars_marhilevics/lesson_7/level_6/CreditCardTest.java
@@ -0,0 +1,7 @@
+package student_einars_marhilevics.lesson_7.level_6;
+
+public class CreditCardTest {
+    public static void main(String[] args) {
+        CreditCardTest creditCardTest = new CreditCardTest();
+    }
+}
