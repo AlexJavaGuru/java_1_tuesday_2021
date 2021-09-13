f28da0bc11f890da6a4d526f88d831fc60afa4e2
Einars <EinarsM@gmail.com>
2021-09-13 13:41:29 +0300

    lesson_7_level_1

--- src/main/java/student_einars_marhilevics/lesson_7/level_1/WordServiceTest.java
+++ src/main/java/student_einars_marhilevics/lesson_7/level_1/WordServiceTest.java
@@ -1,12 +1,14 @@
 package student_einars_marhilevics.lesson_7.level_1;
 
 
+import java.util.Arrays;
+
 class WordServiceTest {
     public static void main(String[] args) {
         WordServiceTest wordServiceTest = new WordServiceTest();
         wordServiceTest.removeSymbolsAndSpaces();
-        /*wordServiceTest.splitString();
-        wordServiceTest.findMostFrequentWord();*/
+        wordServiceTest.splitString();
+        /*wordServiceTest.findMostFrequentWord();*/
     }
     void removeSymbolsAndSpaces() {
         WordService wordService = new WordService();
@@ -18,9 +20,9 @@ class WordServiceTest {
     void splitString() {
         WordService wordService = new WordService();
         String string = "I will go,You will go-but they will stay";
-        //String[] expectedResult = "[I, will, go, You, will, go, but, they, will, stay]"];
-        String result = wordService.removeSymbolsAndSpaces(string);
-        //check(result.equals(expectedResult), "splitString");
+        String[] expectedResult = {"I, will, go, You, will, go, but, they, will, stay"};
+        String[] result = wordService.splitString(string);
+        check(Arrays.equals(result, expectedResult), "splitString");
     }
     void check (boolean condition, String test) {
         if(condition) {
