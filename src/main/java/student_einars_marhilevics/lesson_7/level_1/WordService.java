0afedd30a9533c60eba96c0e504cf901922ca5cf
Einars <EinarsM@gmail.com>
2021-09-13 14:26:03 +0300

    lesson_7_level_1

--- src/main/java/student_einars_marhilevics/lesson_7/level_1/WordService.java
+++ src/main/java/student_einars_marhilevics/lesson_7/level_1/WordService.java
@@ -2,9 +2,10 @@ package student_einars_marhilevics.lesson_7.level_1;
 
 import java.util.Arrays;
 
+
 class WordService {
     String findMostFrequentWord(String text) {
-        String textWithoutSymbols = removeSymbolsAndSpaces(text);
+        String textWithoutSymbols = toLowerCase(text);
         String[] split = splitString(textWithoutSymbols);
         Arrays.sort(split);
         int max = 0;
@@ -12,7 +13,7 @@ class WordService {
         String word = split[0];
         String curr = split[0];
         for (int i = 1; i < split.length; i++) {
-            if (split[i].equals(curr)) {
+            if (split[i].equals(curr) && !split[i].equals("")) {
                 count++;
             } else {
                 count = 1;
@@ -30,7 +31,13 @@ class WordService {
         return str.replaceAll("[^\\dA-Za-z ]", " ");
     }
 
+    String toLowerCase(String str) {
+        String lowerCase = str.toLowerCase();
+        return lowerCase;
+    }
+
     public String[] splitString(String str) {
-        return str.split(" ");
+        String myString = removeSymbolsAndSpaces(str.toLowerCase());
+        return myString.split(" ");
     }
 }
--- src/main/java/student_einars_marhilevics/lesson_7/level_1/WordServiceTest.java
+++ src/main/java/student_einars_marhilevics/lesson_7/level_1/WordServiceTest.java
@@ -7,22 +7,37 @@ class WordServiceTest {
     public static void main(String[] args) {
         WordServiceTest wordServiceTest = new WordServiceTest();
         wordServiceTest.removeSymbolsAndSpaces();
+        wordServiceTest.toLowerCase();
         wordServiceTest.splitString();
-        /*wordServiceTest.findMostFrequentWord();*/
+        wordServiceTest.findMostFrequentWord();
     }
     void removeSymbolsAndSpaces() {
         WordService wordService = new WordService();
-        String string = "I will go,You will go-but they will stay";
-        String expectedResult = "I will go  You will go but they will stay";
+        String string = "I will go,You-will go-but they will,stay";
+        String expectedResult = "I will go You will go but they will stay";
         String result = wordService.removeSymbolsAndSpaces(string);
         check(result.equals(expectedResult), "removeSymbolsAndSpaces");
     }
+    void toLowerCase() {
+        WordService wordService = new WordService();
+        String string = "I Will go,You will.";
+        String expectedResult = "i will go,you will.";
+        String result = wordService.toLowerCase(string);
+        check(result.equals(expectedResult), "toLowerCase");
+    }
     void splitString() {
         WordService wordService = new WordService();
-        String string = "I will go,You will go-but they will stay";
-        String[] expectedResult = {"I, will, go, You, will, go, but, they, will, stay"};
+        String string = "I will go,You will Go-but they Will stay";
+        String[] expectedResult = {"i", "will", "go", "you", "will", "go", "but", "they", "will", "stay"};
         String[] result = wordService.splitString(string);
         check(Arrays.equals(result, expectedResult), "splitString");
+    }
+    void findMostFrequentWord() {
+        WordService wordService = new WordService();
+        String string = "I will go, You You will go-but they you will you stay";
+        String expectedResult = "you";
+        String result = wordService.findMostFrequentWord(string);
+        check(result.equals(expectedResult), "findMostFrequentWord");
     }
     void check (boolean condition, String test) {
         if(condition) {
