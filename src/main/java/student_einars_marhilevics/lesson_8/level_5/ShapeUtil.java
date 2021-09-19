9558771eb19861d366a8a38d445cedf7c3732cfe
Einars <EinarsM@gmail.com>
2021-09-19 21:01:56 +0300

    lesson_8_level_5

---
+++ src/main/java/student_einars_marhilevics/lesson_8/level_5/ShapeUtil.java
@@ -0,0 +1,10 @@
+package student_einars_marhilevics.lesson_8.level_5;
+
+abstract class ShapeUtil {
+    double radius;
+
+    /*public ShapeUtil(double radius) {
+        this.radius = radius;
+    }
+    abstract Circle createRandomCircle();*/
+}
