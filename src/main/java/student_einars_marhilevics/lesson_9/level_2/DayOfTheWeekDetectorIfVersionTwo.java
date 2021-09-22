ffaf4f21854427b82d14e573d284326c9f36b3ca
Einars <EinarsM@gmail.com>
2021-09-22 16:00:20 +0300

    lesson_9_level_2

--- src/main/java/student_einars_marhilevics/lesson_9/level_2/DayOfTheWeekDetectorIfVersionTwo.java
+++ src/main/java/student_einars_marhilevics/lesson_9/level_2/DayOfTheWeekDetectorIfVersionTwo.java
@@ -4,6 +4,25 @@ public class DayOfTheWeekDetectorIfVersionTwo implements DayOfTheWeekDetector {
 
     @Override
     public String detectDayName(int number) {
-        return null;
+        String dayOfTheWeek;
+        switch (number) {
+            case 1: dayOfTheWeek = "Monday";
+                break;
+            case 2: dayOfTheWeek = "Tuesday";
+                break;
+            case 3: dayOfTheWeek = "Wednesday";
+                break;
+            case 4: dayOfTheWeek = "Thursday";
+                break;
+            case 5: dayOfTheWeek = "Friday";
+                break;
+            case 6: dayOfTheWeek = "Saturday";
+                break;
+            case 7: dayOfTheWeek = "Sunday";
+                break;
+            default: dayOfTheWeek = "Please input a valid number between 1 and 7";
+                break;
+        }
+        return dayOfTheWeek;
     }
 }
