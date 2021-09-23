2c318cd49c8e50cc3fdc79c58f15d12c7516b29d
Einars <EinarsM@gmail.com>
2021-09-23 14:15:20 +0300

    lesson_8_level_5

--- src/main/java/student_einars_marhilevics/lesson_8/level_4/ShapeUtilDemo.java
+++ src/main/java/student_einars_marhilevics/lesson_8/level_4/ShapeUtilDemo.java
@@ -1,4 +1,19 @@
 package student_einars_marhilevics.lesson_8.level_4;
 
 public class ShapeUtilDemo {
+    public static void main(String[] args) {
+        ShapeUtil shapeUtil = new ShapeUtil();
+        Shape circle = shapeUtil.createRandomCircle();
+        double circleArea = shapeUtil.calculateArea(circle);
+        double circlePerimeter = shapeUtil.calculatePerimeter(circle);
+        System.out.println("circleArea is " + circleArea);
+        System.out.println("circlePerimeter is " + circlePerimeter);
+
+        ShapeUtil squareTest = new ShapeUtil();
+        Shape square = squareTest.createRandomSquare();
+        double squareArea = squareTest.calculateArea(square);
+        double squarePerimeter = squareTest.calculatePerimeter(square);
+        System.out.println("squareArea is " + squareArea);
+        System.out.println("squarePerimeter is " + squarePerimeter);
+    }
 }
