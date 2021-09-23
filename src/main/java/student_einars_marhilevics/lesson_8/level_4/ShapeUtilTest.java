a42707e64bba5cd20424ebbff088b175c9951cf7
Einars <EinarsM@gmail.com>
2021-09-23 14:57:52 +0300

    lesson_8_level_5

--- src/main/java/student_einars_marhilevics/lesson_8/level_4/ShapeUtilTest.java
+++ src/main/java/student_einars_marhilevics/lesson_8/level_4/ShapeUtilTest.java
@@ -3,23 +3,28 @@ package student_einars_marhilevics.lesson_8.level_4;
 public class ShapeUtilTest {
     public static void main(String[] args) {
         ShapeUtilTest shapeUtilTest = new ShapeUtilTest();
+        shapeUtilTest.calculateArea();
+        shapeUtilTest.calculatePerimeter();
+    }
+    void calculateArea() {
+        ShapeUtil shapeUtil = new ShapeUtil();
         Shape[] shapes = new Shape[4];
         for (int i = 0; i < shapes.length; i++) {
-            shapes[i] = createRandomShape;
+            shapes[i] = shapeUtil.createRandomShape();
+
+            double result = shapeUtil.calculateArea(shapes);
+            check(result == 0, "calculateArea ");
         }
-        shapeUtilTest.calculateArea(shapes);
-        shapeUtilTest.calculatePerimeter(shapes);
-    }
-    void calculateArea(Shape[] shapes) {
-        ShapeUtil shapeUtil = new ShapeUtil();
-        double result = shapeUtil.calculateArea(shapes);
-        check (result == 0, "calculateArea ");
     }
 
-    void calculatePerimeter(Shape[] shapes) {
+    void calculatePerimeter() {
         ShapeUtil shapeUtil = new ShapeUtil();
-        double result = shapeUtil.calculatePerimeter(shapes);
-        check (result == 0, "calculatePerimeter ");
+        Shape[] shapes = new Shape[4];
+        for (int i = 0; i < shapes.length; i++) {
+            shapes[i] = shapeUtil.createRandomShape();
+            double result = shapeUtil.calculatePerimeter(shapes);
+            check(result != 0, "calculatePerimeter ");
+        }
     }
 
     void check (boolean condition, String testName) {
