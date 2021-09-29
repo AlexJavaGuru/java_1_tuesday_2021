f880b5e7111933967b3b613bd16429dec2a15717
Einars <EinarsM@gmail.com>
2021-09-29 16:59:38 +0300

    lesson_7_level_7

--- src/main/java/student_einars_marhilevics/lesson_7/level_7/WordService.java
+++ src/main/java/student_einars_marhilevics/lesson_7/level_7/WordService.java
@@ -3,12 +3,15 @@ package student_einars_marhilevics.lesson_7.level_7;
 class WordService {
     public String findMostFrequentWord(String text) {
         String myText = text.toLowerCase();
-        String[] textArray;
-        int[] wordCountArray;
-        textArray = myText.split(" ");
-        wordCountArray = new int[textArray.length];
+        String[] textArray = myText.split(" ");
+        int[] wordCountArray = wordsAmount(textArray);
+        int index = getIndexOfWord(wordCountArray);
+        return textArray[index];
+    }
 
+    private int[] wordsAmount(String[] textArray) {
         int wordCounter = 0;
+        int[] wordCountArray = new int[textArray.length];
         for (int wordCount = 0; wordCount < textArray.length; wordCount++) {
             for (String s : textArray) {
                 if (textArray[wordCount].equals(s) && !textArray[wordCount].equals("")) {
@@ -18,13 +21,16 @@ class WordService {
             wordCountArray[wordCount] = wordCounter;
             wordCounter = 0;
         }
+        return wordCountArray;
+    }
 
+    private int getIndexOfWord(int[] textArray) {
         int max = 0;
-        for (int count = 0; count < wordCountArray.length; count++) {
-            if (wordCountArray[count] > max) {
-                max = count;
+        for (int i = 0; i < textArray.length; i++) {
+            if (textArray[i] > max) {
+                max = i;
             }
         }
-        return textArray[max];
+        return max;
     }
 }
