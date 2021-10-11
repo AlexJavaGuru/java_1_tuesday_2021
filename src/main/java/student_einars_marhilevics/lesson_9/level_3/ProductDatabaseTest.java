1be65d9cbea3ec702e7dd0462e9106e9e3e40c74
Einars <EinarsM@gmail.com>
2021-10-11 19:50:16 +0300

    lesson_9_level_3

--- src/main/java/student_einars_marhilevics/lesson_9/level_3/ProductDatabaseTest.java
+++ src/main/java/student_einars_marhilevics/lesson_9/level_3/ProductDatabaseTest.java
@@ -1,10 +1,14 @@
 package student_einars_marhilevics.lesson_9.level_3;
 
+import java.util.Optional;
+
 public class ProductDatabaseTest {
     public static void main(String[] args) {
         ProductDatabaseTest productDatabaseTest = new ProductDatabaseTest();
         productDatabaseTest.save();
         productDatabaseTest.findByTitle();
+        productDatabaseTest.saveOptional();
+        productDatabaseTest.findByTitleOptional();
     }
 
     void save() {
@@ -21,6 +25,22 @@ public class ProductDatabaseTest {
         inMemoryDatabase.save(product);
         Product result = inMemoryDatabase.findByTitle("Apple");
         check(result == product, "Test FIND");
+    }
+
+    void saveOptional() {
+        InMemoryDatabaseOptional inMemoryDatabaseOptional = new InMemoryDatabaseOptional();
+        Product product = new Product("Apple");
+        inMemoryDatabaseOptional.save(product);
+        Product result = inMemoryDatabaseOptional.products[0];
+        check(result == product, "Test Optional SAVE");
+    }
+
+    void findByTitleOptional() {
+        InMemoryDatabaseOptional inMemoryDatabaseOptional = new InMemoryDatabaseOptional();
+        Product product = new Product("Apple");
+        inMemoryDatabaseOptional.save(product);
+        Optional<Product> result = inMemoryDatabaseOptional.findByTitle("Apple");
+        check(result.equals(Optional.of(product)), "Test Optional FIND");
     }
 
 
