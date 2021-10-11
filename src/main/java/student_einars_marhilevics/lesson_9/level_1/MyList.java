63229cdf4c80a6b7567f30fd72d53eb32f5c96ad
Einars <EinarsM@gmail.com>
2021-10-11 14:37:40 +0300

    lesson_9_level_2

--- src/main/java/student_einars_marhilevics/lesson_9/level_1/MyList.java
+++
@@ -1,5 +0,0 @@
-package student_einars_marhilevics.lesson_9.level_1;
-
-public interface MyList {
-
-}
--- src/main/java/student_einars_marhilevics/lesson_9/level_2/DayOfTheWeekDetectorDemo.java
+++ src/main/java/student_einars_marhilevics/lesson_9/level_2/DayOfTheWeekDetectorDemo.java
@@ -3,11 +3,28 @@ package student_einars_marhilevics.lesson_9.level_2;
 import java.util.Scanner;
 
 public class DayOfTheWeekDetectorDemo {
+
+    private DayOfTheWeekDetector dayOfTheWeekDetector;
+
+    public DayOfTheWeekDetectorDemo(DayOfTheWeekDetector dayOfTheWeekDetector) {
+        this.dayOfTheWeekDetector = dayOfTheWeekDetector;
+    }
     public static void main(String[] args) {
-        DayOfTheWeekDetectorIfVersionTwo dayOfTheWeekDetectorIfVersionTwo = new DayOfTheWeekDetectorIfVersionTwo();
+
+        DayOfTheWeekDetectorDemo versionOne = new DayOfTheWeekDetectorDemo(new DayOfTheWeekDetectorIfVersion());
+        versionOne.run();
+
+        DayOfTheWeekDetectorDemo versionTwo = new DayOfTheWeekDetectorDemo(new DayOfTheWeekDetectorIfVersionTwo());
+        versionTwo.run();
+
+        DayOfTheWeekDetectorDemo versionThree = new DayOfTheWeekDetectorDemo(new DayOfTheWeekDetectorIfVersionThree());
+        versionThree.run();
+
+    }
+    private void run() {
         Scanner myScanner = new Scanner(System.in);
-        System.out.println("Enter number of day of the week");
+        System.out.print("Enter number of day of the week");
         int dayOfTheWeek = myScanner.nextInt();
-        System.out.println(dayOfTheWeekDetectorIfVersionTwo.detectDayName(dayOfTheWeek));
+        System.out.println("Day of the week: " + dayOfTheWeekDetector.detectDayName(dayOfTheWeek));
     }
 }
