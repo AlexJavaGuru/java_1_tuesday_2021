c12a4640fe0c42731d674a43078880098886ecde
Einars <EinarsM@gmail.com>
2021-10-12 10:52:12 +0300

    lesson_10_level_1

---
+++ src/main/java/student_einars_marhilevics/lesson_10/level_1/SafeVolTwo.java
@@ -0,0 +1,11 @@
+package student_einars_marhilevics.lesson_10.level_1;
+
+class SafeVolTwo {
+    int pinCode;
+    int amount;
+
+    SafeVolTwo(int pinCode, int amount) {
+        this.pinCode = pinCode;
+        this.amount = amount;
+    }
+}
---
+++ src/main/java/student_einars_marhilevics/lesson_10/level_1/SafeVolTwoDemo.java
@@ -0,0 +1,4 @@
+package student_einars_marhilevics.lesson_10.level_1;
+
+class SafeVolTwoDemo {
+}
