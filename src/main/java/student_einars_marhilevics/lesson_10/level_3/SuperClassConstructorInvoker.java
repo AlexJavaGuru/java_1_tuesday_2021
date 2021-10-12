51f224bc2c4e2e3192ff7d937f88e4755d8d2145
Einars <EinarsM@gmail.com>
2021-10-12 22:03:58 +0300

    lesson_10_level_3

---
+++ src/main/java/student_einars_marhilevics/lesson_10/level_3/SuperClassConstructorInvoker.java
@@ -0,0 +1,9 @@
+package student_einars_marhilevics.lesson_10.level_3;
+
+public class SuperClassConstructorInvoker {
+    public SuperClassConstructorInvoker() {
+        System.out.println(1);
+        //super();
+        System.out.println(2);
+    }
+}
