bced1a818f65368ffd5d6fb2e8eee70f74cfbf73
Olegs-github <oleg-k@inbox.lv>
2021-10-17 13:14:34 +0300

    Lesson 12: initial commit

---
+++ src/main/java/student_oleg_kozlov/lesson_12/level_5/ValidationException.java
@@ -0,0 +1,36 @@
+package student_oleg_kozlov.lesson_12.level_5;
+
+import java.util.Objects;
+
+// Task_30, Task_31
+class ValidationException extends Exception {
+    private String ruleName;
+    private String description;
+    private String fieldName;
+
+    public ValidationException(String ruleName, String description, String fieldName) {
+        this.ruleName = ruleName;
+        this.description = description;
+        this.fieldName = fieldName;
+    }
+
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ValidationException that = (ValidationException) o;
+        return Objects.equals(ruleName, that.ruleName) && Objects.equals(description, that.description) && Objects.equals(fieldName, that.fieldName);
+    }
+}
