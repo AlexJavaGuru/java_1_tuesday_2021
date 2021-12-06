fd63ed856e0876a2144161356fe787556bc33145
zilite <pepellv@inbox.lv>
2021-12-06 20:43:01 +0200

    ispravleno

--- src/main/java/student_sergej_pereligin/lesson_6/level_1/NumberUtilDemo.java
+++ src/main/java/student_sergej_pereligin/lesson_6/level_1/NumberUtilDemo.java
@@ -1,22 +1,22 @@
 package student_sergej_pereligin.lesson_6.level_1;
 
-public class NumberUtilDemo {   public static void main(String[] args) {
-    NumberUtilDemo numberUtilsTest = new NumberUtilDemo();
-    numberUtilsTest.isEvenTest();
+public class NumberUtilDemo {
+    public static void main(String[] args) {
+        NumberUtilDemo numberUtilsTest = new NumberUtilDemo();
+        numberUtilsTest.isEvenTest();
 
-}
+    }
 
-    void isEvenTest(){
+    void isEvenTest() {
         NumberUtil numberUtils = new NumberUtil();
         boolean result = numberUtils.isEven(4);
         checkRecults(result, "isEvenTest");
     }
 
-    void checkRecults(boolean condition, String testName){
+    void checkRecults(boolean condition, String testName) {
         if (condition) {
             System.out.println("Test " + testName + " passed!");
-        }
-        else {
+        } else {
             System.out.println("Test " + testName + "  failed!");
         }
     }
